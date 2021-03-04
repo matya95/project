69838f6c839eed207ed5f2cdef087cb20c4ff105
Matyasovszki Máté <matya012@gmail.com>
2021-03-04 06:32:54 +0100

    Select

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -3,11 +3,11 @@
     <div class="container">
         {{Form::open(['route' => ['projects.update',$project->id],'method' => 'POST'])}}
         {{Form::label('email', 'Név')}}
-        {{Form::text('name','',['class'=>'form-control'])}}
+        {{Form::text('name',$project->name,['class'=>'form-control'])}}
         {{Form::label('description', 'Leírás')}}
-        {{Form::textarea('description','',['class'=>'form-control'])}}
+        {{Form::textarea('description',$project->description,['class'=>'form-control'])}}
         {{Form::label('description', 'Státusz')}}
-        {{Form::select('status', ['not dev'=>'Fejlesztésre vár', 'developing' => 'Folyamatban','ready'=>'kész'],null,['class'=>'form-control'])}}
+        {{Form::select('status', ['not dev'=>'Fejlesztésre vár', 'developing' => 'Folyamatban','ready'=>'kész'],$project->status,['class'=>'form-control'])}}
        {{Form::hidden('_method','PUT')}}
         {{Form::submit('Küldés!',['class'=>'btn btn-primary mt-4'])}}
     </div>
